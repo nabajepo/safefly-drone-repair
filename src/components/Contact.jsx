2d29c5ec7a891211d5fb72001f14c6c86b7d1e77
L <145072090+nabajepo@users.noreply.github.com>
2026-06-05 18:58:50 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,14 +9,16 @@ function Contact() {
 
         <div className="contact-box">
 
+          {/* Address */}
           <div className="contact-item">
             <h4>Address</h4>
             <p>
-              75 Laurier Ave E,
+              75 Laurier Ave E <br />
               Ottawa, ON K1N 6N5
             </p>
           </div>
 
+          {/* Phone */}
           <div className="contact-item">
             <h4>Phone</h4>
             <p>
@@ -24,10 +26,34 @@ function Contact() {
             </p>
           </div>
 
+          {/* Email */}
           <div className="contact-item">
             <h4>Email</h4>
             <p>
               contact@safeflyrepair.ca
+            </p>
+          </div>
+
+          {/* Business Hours */}
+          <div className="contact-item">
+            <h4>Business Hours</h4>
+
+            <p>
+              <strong>Monday - Friday</strong>
+              <br />
+              8:00 AM - 6:00 PM
+            </p>
+
+            <p>
+              <strong>Saturday</strong>
+              <br />
+              9:00 AM - 2:00 PM
+            </p>
+
+            <p>
+              <strong>Sunday</strong>
+              <br />
+              Closed
             </p>
           </div>
 
